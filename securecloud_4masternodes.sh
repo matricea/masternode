4067381686d93b5c9ff78076b4e0cb8cc9b051a7
matricea <38187062+matricea@users.noreply.github.com>
2018-11-19 17:59:39 +0100

    donation text updated:

--- securecloud_4masternodes.sh
+++ securecloud_4masternodes.sh
@@ -166,4 +166,7 @@ until securecloud-cli -datadir=/home/securecloud4/.securecloud mnsync status | g
 echo -e ${GREEN}"Last node is fully synced. You fourth masternode is running!"${NC}
 echo ""
 echo -e ${GREEN}"Congrats! Your SecureCloud Masternodes are now installed and started. Please wait from 10-20 minutes in order to give the masternode enough time to sync, then start the node from your wallet, Debug console option"${NC}
-echo "For donations: sUhHTix3QzMLFKKGiQ5U7s9MLJNopr3M6B";
+echo "If you think that this tutorial helped in some way, feel free to donate for our work:"
+echo "SecureCloudNet address: sUhHTix3QzMLFKKGiQ5U7s9MLJNopr3M6B"
+echo "LTC address: LbF8hSejc8oc4L81CrzdYengYBpr6xNczn"
+echo "The END. You can close now the SSH terminal session";
