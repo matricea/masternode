6c8d1b9ed3e9e234864f9d260fedadfc5fd28648
matricea <38187062+matricea@users.noreply.github.com>
2018-12-01 22:19:03 +0100

    Update kingscoin_4masternodes.sh

--- kingscoin_4masternodes.sh
+++ kingscoin_4masternodes.sh
@@ -28,11 +28,11 @@ echo -e ${GREEN}"Please Enter Your Masternodes Private Key for the third node:"$
 read privkey3
 echo -e ${GREEN}"Please Enter Your Masternodes Private Key for 4th node:"${NC}
 read privkey4
-echo "Creating 4 SecureCloud system users with no-login access:"
-sudo adduser --system --home /home/kgs kgs
-sudo adduser --system --home /home/kgs2 kgs2
-sudo adduser --system --home /home/kgs3 kgs3
-sudo adduser --system --home /home/kgs4 kgs4
+echo "Creating 4 KingsCoin system users with no-login access:"
+sudo adduser --system --home /home/kingscoin kingscoin
+sudo adduser --system --home /home/kingscoin2 kingscoin2
+sudo adduser --system --home /home/kingscoin3 kingscoin3
+sudo adduser --system --home /home/kingscoin4 kingscoin4
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install software-properties-common
@@ -58,115 +58,115 @@ sudo mkswap /var/swap.img
 sudo swapon /var/swap.img
 sudo echo ' /var/swap.img none swap sw 0 0 ' >> /etc/fstab
 cd ~
-sudo mkdir /root/kgs
-cd /root/kgs
+sudo mkdir /root//kingscoin
+cd /root/kingscoin
 wget https://github.com/kingscrypto/KINGSCOIN/releases/download/1.0.0/kingscoin-1.0.0-x86_64-linux-gnu.tar.gz
 tar -xzvf kingscoin-1.0.0-x86_64-linux-gnu.tar.gz
-sudo mv /root/kgs/kingscoind /root/kgs/kingscoin-cli /root/kgs/kingscoin-tx /usr/local/bin
-sudo chmod 755 -R  /usr/local/bin/securecloud*
-sudo mkdir /home/securecloud/.securecloud
-sudo touch /home/securecloud/.securecloud/securecloud.conf
+sudo mv /root/kingscoin/kingscoind /root/kingscoin/kingscoin-cli /root/kingscoin/kingscoin-tx /usr/local/bin
+sudo chmod 755 -R  /usr/local/bin/kingscoin*
+sudo mkdir /home/kingscoin/.kingscoin
+sudo touch /home/kingscoin/.kingscoin/kingscoin.conf
 echo -e "${GREEN}Configuring Wallet for first node${NC}"
-echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> /home/securecloud/.securecloud/securecloud.conf
-echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> /home/securecloud/.securecloud/securecloud.conf
-echo "rpcallowip=127.0.0.1" >> /home/securecloud/.securecloud/securecloud.conf
-echo "server=1" >> /home/securecloud/.securecloud/securecloud.conf
-echo "daemon=1" >> /home/securecloud/.securecloud/securecloud.conf
-echo "maxconnections=250" >> /home/securecloud/.securecloud/securecloud.conf
-echo "masternode=1" >> /home/securecloud/.securecloud/securecloud.conf
-echo "rpcport=9191" >> /home/securecloud/.securecloud/securecloud.conf
-echo "listen=0" >> /home/securecloud/.securecloud/securecloud.conf
-echo "externalip=$(hostname  -I | cut -f1 -d' '):9191" >> /home/securecloud/.securecloud/securecloud.conf
-echo "masternodeprivkey=$privkey" >> /home/securecloud/.securecloud/securecloud.conf
-echo "addnode=93.103.247.155" >> /home/securecloud/.securecloud/securecloud.conf
-echo "addnode=80.211.19.145" >> /home/securecloud/.securecloud/securecloud.conf
-echo "addnode=209.250.252.123" >> /home/securecloud/.securecloud/securecloud.conf
-echo "addnode=128.199.164.15" >> /home/securecloud/.securecloud/securecloud.conf
+echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "rpcallowip=127.0.0.1" >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "server=1" >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "daemon=1" >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "maxconnections=250" >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "masternode=1" >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "rpcport=1778" >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "listen=0" >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "externalip=$(hostname  -I | cut -f1 -d' '):1777" >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "masternodeprivkey=$privkey" >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "addnode=80.211.174.239" >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "addnode=80.211.45.25" >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "addnode=80.211.40.41" >> /home/kingscoin/.kingscoin/kingscoin.conf
+echo "addnode=185.43.210.213" >> /home/kingscoin/.kingscoin/kingscoin.conf
 sleep 5
 echo -e "${GREEN}Configuring Wallet for second node${NC}"
-sudo mkdir /home/securecloud2/.securecloud
-sudo touch /home/securecloud2/.securecloud/securecloud.conf
-echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> /home/securecloud2/.securecloud/securecloud.conf
-echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> /home/securecloud2/.securecloud/securecloud.conf
-echo "rpcallowip=127.0.0.1" >> /home/securecloud2/.securecloud/securecloud.conf
-echo "server=1" >> /home/securecloud2/.securecloud/securecloud.conf
-echo "daemon=1" >> /home/securecloud2/.securecloud/securecloud.conf
-echo "maxconnections=250" >> /home/securecloud2/.securecloud/securecloud.conf
-echo "masternode=1" >> /home/securecloud2/.securecloud/securecloud.conf
-echo "rpcport=9192" >> /home/securecloud2/.securecloud/securecloud.conf
-echo "listen=0" >> /home/securecloud2/.securecloud/securecloud.conf
-echo "externalip=$(hostname  -I | cut -f1 -d' '):9191" >> /home/securecloud2/.securecloud/securecloud.conf
-echo "masternodeprivkey=$privkey2" >> /home/securecloud2/.securecloud/securecloud.conf
-echo "addnode=93.103.247.155" >> /home/securecloud2/.securecloud/securecloud.conf
-echo "addnode=80.211.19.145" >> /home/securecloud2/.securecloud/securecloud.conf
-echo "addnode=209.250.252.123" >> /home/securecloud2/.securecloud/securecloud.conf
-echo "addnode=128.199.164.15" >> /home/securecloud2/.securecloud/securecloud.conf
+sudo mkdir /home/kingscoin2/.kingscoin
+sudo touch /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "rpcallowip=127.0.0.1" >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "server=1" >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "daemon=1" >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "maxconnections=250" >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "masternode=1" >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "rpcport=1779" >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "listen=0" >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "externalip=$(hostname  -I | cut -f1 -d' '):1777" >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "masternodeprivkey=$privkey2" >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "addnode=80.211.174.239" >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "addnode=80.211.45.25" >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "addnode=80.211.40.41" >> /home/kingscoin2/.kingscoin/kingscoin.conf
+echo "addnode=185.43.210.213" >> /home/kingscoin2/.kingscoin/kingscoin.conf
 sleep 5
 echo -e "${GREEN}Configuring Wallet for third node${NC}"
-sudo mkdir /home/securecloud3/.securecloud
-sudo touch /home/securecloud3/.securecloud/securecloud.conf
-echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> /home/securecloud3/.securecloud/securecloud.conf
-echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> /home/securecloud3/.securecloud/securecloud.conf
-echo "rpcallowip=127.0.0.1" >> /home/securecloud3/.securecloud/securecloud.conf
-echo "server=1" >> /home/securecloud3/.securecloud/securecloud.conf
-echo "daemon=1" >> /home/securecloud3/.securecloud/securecloud.conf
-echo "maxconnections=250" >> /home/securecloud3/.securecloud/securecloud.conf
-echo "masternode=1" >> /home/securecloud3/.securecloud/securecloud.conf
-echo "rpcport=9193" >> /home/securecloud3/.securecloud/securecloud.conf
-echo "listen=0" >> /home/securecloud3/.securecloud/securecloud.conf
-echo "externalip=[$(hostname  -I | cut -f2 -d' ')]:9191" >> /home/securecloud3/.securecloud/securecloud.conf
-echo "masternodeprivkey=$privkey3" >> /home/securecloud3/.securecloud/securecloud.conf
-echo "addnode=93.103.247.155" >> /home/securecloud3/.securecloud/securecloud.conf
-echo "addnode=80.211.19.145" >> /home/securecloud3/.securecloud/securecloud.conf
-echo "addnode=209.250.252.123" >> /home/securecloud3/.securecloud/securecloud.conf
-echo "addnode=128.199.164.15" >> /home/securecloud3/.securecloud/securecloud.conf
+sudo mkdir /home/kingscoin3/.kingscoin
+sudo touch /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "rpcallowip=127.0.0.1" >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "server=1" >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "daemon=1" >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "maxconnections=250" >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "masternode=1" >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "rpcport=1780" >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "listen=0" >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "externalip=[$(hostname  -I | cut -f2 -d' ')]:1777" >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "masternodeprivkey=$privkey3" >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "addnode=80.211.174.239" >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "addnode=80.211.45.25" >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "addnode=80.211.40.41" >> /home/kingscoin3/.kingscoin/kingscoin.conf
+echo "addnode=185.43.210.213" >> /home/kingscoin3/.kingscoin/kingscoin.conf
 sleep 5
 echo -e "${GREEN}Configuring Wallet for 4th node${NC}"
-sudo mkdir /home/securecloud4/.securecloud
-sudo touch /home/securecloud4/.securecloud/securecloud.conf
-echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> /home/securecloud4/.securecloud/securecloud.conf
-echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> /home/securecloud4/.securecloud/securecloud.conf
-echo "rpcallowip=127.0.0.1" >> /home/securecloud4/.securecloud/securecloud.conf
-echo "server=1" >> /home/securecloud4/.securecloud/securecloud.conf
-echo "daemon=1" >> /home/securecloud4/.securecloud/securecloud.conf
-echo "maxconnections=250" >> /home/securecloud4/.securecloud/securecloud.conf
-echo "masternode=1" >> /home/securecloud4/.securecloud/securecloud.conf
-echo "rpcport=9194" >> /home/securecloud4/.securecloud/securecloud.conf
-echo "listen=0" >> /home/securecloud4/.securecloud/securecloud.conf
-echo "externalip=[$(hostname  -I | cut -f2 -d' ')]:9191" >> /home/securecloud4/.securecloud/securecloud.conf
-echo "masternodeprivkey=$privkey4" >> /home/securecloud4/.securecloud/securecloud.conf
-echo "addnode=93.103.247.155" >> /home/securecloud4/.securecloud/securecloud.conf
-echo "addnode=80.211.19.145" >> /home/securecloud4/.securecloud/securecloud.conf
-echo "addnode=209.250.252.123" >> /home/securecloud4/.securecloud/securecloud.conf
-echo "addnode=128.199.164.15" >> /home/securecloud4/.securecloud/securecloud.conf
+sudo mkdir /home/kingscoin4/.kingscoin
+sudo touch /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "rpcallowip=127.0.0.1" >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "server=1" >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "daemon=1" >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "maxconnections=250" >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "masternode=1" >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "rpcport=1781" >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "listen=0" >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "externalip=[$(hostname  -I | cut -f2 -d' ')]:1777" >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "masternodeprivkey=$privkey4" >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "addnode=80.211.174.239" >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "addnode=80.211.45.25" >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "addnode=80.211.40.41" >> /home/kingscoin4/.kingscoin/kingscoin.conf
+echo "addnode=185.43.210.213" >> /home/kingscoin4/.kingscoin/kingscoin.conf
 sleep 5
 fi
 echo "Syncing first node, please wait...";
-securecloudd -datadir=/home/securecloud/.securecloud -daemon
+kingscoind -datadir=/home/kingscoin/.kingscoin -daemon
 sleep 10
-until securecloud-cli -datadir=/home/securecloud/.securecloud mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
+until kingscoin-cli -datadir=/home/kingscoin/.kingscoin mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
 echo -e ${GREEN}"First node is fully synced. You 1st masternode is running!"${NC}
 sleep 10
 echo "Syncing second node, please wait...";
-securecloudd -datadir=/home/securecloud2/.securecloud -daemon
+kingscoind -datadir=/home/kingscoin2/.kingscoin -daemon
 sleep 10
-until securecloud-cli -datadir=/home/securecloud2/.securecloud mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
+until kingscoin-cli -datadir=/home/kingscoin2/.kingscoin mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
 echo -e ${GREEN}"Second node is fully synced. You second masternode is running!"${NC}
 sleep 10
 echo "Syncing third node, please wait...";
-securecloudd -datadir=/home/securecloud3/.securecloud -daemon
+kingscoind -datadir=/home/kingscoin3/.kingscoin -daemon
 sleep 10
-until securecloud-cli -datadir=/home/securecloud3/.securecloud mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
+until kingscoin-cli -datadir=/home/kingscoin3/.kingscoin mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
 echo -e ${GREEN}"Third node is fully synced. You third masternode is running!"${NC}
 sleep 10
 echo "Syncing fourth node, please wait...";
-securecloudd -datadir=/home/securecloud4/.securecloud -daemon
+kingscoind -datadir=/home/kingscoin4/.kingscoin -daemon
 sleep 10
-until securecloud-cli -datadir=/home/securecloud4/.securecloud mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
+until kingscoin-cli -datadir=/home/kingscoin4/.kingscoin mnsync status | grep -m 1 '"IsBlockchainSynced": true,'; do sleep 1 ; done > /dev/null 2>&1
 echo -e ${GREEN}"Last node is fully synced. You fourth masternode is running!"${NC}
 echo ""
 echo -e ${GREEN}"Congrats! Your KingsCoin Masternodes are now installed and started. Please wait from 10-20 minutes in order to give the masternode enough time to sync, then start the node from your wallet, Debug console option"${NC}
 echo "If you think that this tutorial helped in some way, feel free to donate for our work:"
-echo "SecureCloudNet address: sUhHTix3QzMLFKKGiQ5U7s9MLJNopr3M6B"
+echo "KingsCoins address: JFheuYTX3HjCEuWhZdHj1wp9mzkFezdKen"
 echo "LTC address: LbF8hSejc8oc4L81CrzdYengYBpr6xNczn"
 echo "The END. You can close now the SSH terminal session";
